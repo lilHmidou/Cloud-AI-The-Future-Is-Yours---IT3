d57c4a658482e9d1c1a2958fd886c4004ef3a72d
lilHmidou <ahmedaminesahnoun@gmail.com>
2026-01-27 13:18:46 +0100

    fix error : vm

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,7 @@
 output "project_id" {
   value = "it3-terraform-tp"
 }
+
+output "public_ip" {
+    value = google_compute_address.public_ip.address
+}
--- terraform/vm.tf
+++ terraform/vm.tf
@@ -1,11 +1,10 @@
 resource "google_compute_address" "public_ip" {
-    for_each = local.roles
-    name = "${local.name_prefix}-nginx-${each.key}-ip"
-    region   = var.region
+  name = "tp-nginx-static-ip"
+  region = var.region
 }
-resource "google_compute_instance" "vm" {
-  for_each     = local.roles
-  name         = "${local.name_prefix}-${each.key}"
+
+resource "google_compute_instance" "nginx_vm" {
+  name         = "nginx-vm"
   machine_type = "e2-micro"
   zone         = var.zone
 
@@ -15,14 +14,15 @@ resource "google_compute_instance" "vm" {
     }
   }
 
-  metadata = {
-    ssh-keys = "${var.ssh_user}:${file(pathexpand("~/.ssh/id_ed25519.pub"))}"
-  }
-
   network_interface {
     subnetwork = google_compute_subnetwork.subnet.id
+
     access_config {
-      nat_ip = google_compute_address.public_ip[each.key].address
+      nat_ip = google_compute_address.public_ip.address
     }
+  }
+
+  metadata = {
+    ssh-keys = "${var.ssh_user}:${file("~/.ssh/id_ed25519.pub")}"
   }
 }
