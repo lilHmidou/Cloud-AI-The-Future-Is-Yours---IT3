56a2f5fc094f176e02962956eda09864accafccd
lilHmidou <ahmedaminesahnoun@gmail.com>
2026-01-27 12:57:32 +0100

    fix error : multiple port

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -1,27 +1,28 @@
-resource "google_compute_address" "static_ip" {
-  name = "nginx-static-ip"
+resource "google_compute_address" "public_ip" {
+    for_each = local.roles
+    name = "${local.name_prefix}-nginx-${each.key}-ip"
+    region   = var.region
 }
-
-resource "google_compute_instance" "nginx_vm" {
-  name         = "nginx-vm"
+resource "google_compute_instance" "vm" {
+  for_each     = local.roles
+  name         = "${local.name_prefix}-${each.key}"
   machine_type = "e2-micro"
   zone         = var.zone
 
   boot_disk {
     initialize_params {
-      image = "debian-12-bookworm-v20240515"
-    }
-  }
-
-  network_interface {
-    subnetwork = google_compute_subnetwork.subnet.id
-
-    access_config {
-      nat_ip = google_compute_address.static_ip.address
+      image = "debian-cloud/debian-12"
     }
   }
 
   metadata = {
-    ssh-keys = "${var.ssh_user}:${file("~/.ssh/id_ed25519.pub")}"
+    ssh-keys = "${var.ssh_user}:${file(pathexpand("~/.ssh/id_ed25519.pub"))}"
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.subnet.id
+    access_config {
+      nat_ip = google_compute_address.public_ip[each.key].address
+    }
   }
 }
